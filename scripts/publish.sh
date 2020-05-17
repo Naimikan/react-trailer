105ce8c52cdb1200f91cbba12b97985370247f63
Naimikan Kimn <guillem.aldeguer@gmail.com>
2020-05-17 19:13:52 +0200

    fix: fix

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -14,25 +14,3 @@ if [ $CURRENT_BRANCH == 'master' ]
 then
   lerna publish --conventional-commits --yes
 fi
-
-# Update development with master
-if [ $CURRENT_BRANCH == 'master' ]
-then
-  git checkout development
-  git fetch --all
-  git reset --hard origin/$CURRENT_BRANCH
-  git push
-fi
-
-# Update master with development
-if [ $CURRENT_BRANCH == 'development' ]
-then
-  git checkout master
-  git fetch --all
-  git fetch --all
-  git reset --hard origin/$CURRENT_BRANCH
-  git push
-fi
-
-# Return to the current branch
-git checkout $CURRENT_BRANCH
