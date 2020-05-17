a814165293fa1640fdab39afd61cfaaf4a6b60c4
Naimikan Kimn <guillem.aldeguer@gmail.com>
2020-05-17 18:32:09 +0200

    fix: fixed publish process

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,6 +3,8 @@ set -e
 
 CURRENT_BRANCH=($(git branch --show-current))
 
+cp -f ../README.md ../packages/video-player/README.md
+
 if [ $CURRENT_BRANCH == 'development' ]
 then
   lerna publish --conventional-commits --canary --preid beta --dist-tag next --no-git-reset --yes
