9642e7f38ff13754cdc6a209e48db59b95c08f68
Naimikan Kimn <guillem.aldeguer@gmail.com>
2020-05-31 21:11:50 +0200

    fix: added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,11 @@ module.exports = {
   plugins: [
     'gatsby-theme-docz',
     'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-168112279-1',
+      },
+    },
   ],
 };
